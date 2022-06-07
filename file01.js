9eaf9c359751383d2e6acf630c0aec35b2da1e91
unknown <rakinuzzaman@gmail.com>
2022-06-07 19:51:29 +0600

    32th task

--- file01.js
+++ file01.js
@@ -124,4 +124,60 @@ a /= 12;
 b /= 4;
 c /= 11;
 
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+const myStr = "This is the start. " + "This is the end."; // Change this line
+
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+
+// Only change code below this line
+const myName = "Rakin";
+const myStr = "My name is " + myName + "and I am well!";
+
+// Change code below this line
+const someAdjective = "hard";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective;
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
 
