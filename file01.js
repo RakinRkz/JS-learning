3f0b9a95c42e0b1d791bbad27f3c22eacd0dba0b
unknown <rakinuzzaman@gmail.com>
2022-06-07 18:45:58 +0600

    21th task

--- file01.js
+++ file01.js
@@ -48,3 +48,80 @@ studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
 
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+const sum = 10 + 10;
+
+const difference = 45 - 33;
+
+const product = 8 * 10;
+
+const quotient = 66 / 33;
+
+let myVar = 87;
+
+// Only change code below this line
+myVar ++;
+
+let myVar = 11;
+
+// Only change code below this line
+myVar --;
+
+
+const ourDecimal = 5.7;
+
+// Only change code below this line
+const myDecimal = 1.23;
+
+const product = 2.0 * 2.5;
+
+const quotient = 4.4 / 2.0; // Change this line
+
+const remainder = 11 % 3;
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+
