dae6f5924009a0ff4531e7c2abda444df2826bf6
unknown <rakinuzzaman@gmail.com>
2022-06-07 18:28:26 +0600

    many tasks later

--- file01.js
+++ file01.js
@@ -20,3 +20,31 @@ var b;
 b = a;
 
 
+var a = 9;
+
+
+var myFirstName = "Rakinuzzaman";
+var myLastName = "AKM";
+
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a = 5;
+b = 10;
+c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
