34b5f573b11f391d39c84b7d2ca4bfa3b5c74dd0
rakinrkz <rakinuzzaman@gmail.com>
2022-06-14 18:35:35 +0600

    after 100% done all 113 tasks

--- file01.js
+++ file01.js
@@ -983,3 +983,118 @@ function sum(arr, n) {
   // Only change code above this line
 }
 
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for(let i = 0; i< contacts.length; i++)
+  {
+    if(contacts[i].firstName === name)
+    {
+      if(contacts[i].hasOwnProperty(prop))
+      return contacts[i][prop];
+      else
+      return "No such property";
+    }
+  }
+  return "No such contact"
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
+
+function randomFraction() {
+
+  // Only change code below this line
+
+  return Math.random();
+
+  // Only change code above this line
+}
+
+function randomWholeNum() {
+
+  // Only change code below this line
+
+  return Math.floor(Math.random() * 10);
+}
+
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  // Only change code above this line
+}
+
+function convertToInteger(str) {
+  return parseInt(str);
+}
+
+convertToInteger("56");
+
+function convertToInteger(str) {
+  return parseInt(str, 2);
+}
+
+convertToInteger("10011");
+
+function checkEqual(a, b) {
+  return a == b ? "Equal" : "Not Equal";
+ }
+ 
+ checkEqual(1, 2);
+
+ function checkSign(num) {
+  return num > 0 ? "positive" : num == 0 ? "zero" : "negative";
+
+}
+
+checkSign(10);
+
+// Only change code below this line
+function countdown(n){
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countdown(n - 1);
+    countArray.unshift(n);
+    return countArray;
+  }
+}
+// Only change code above this line
+
+
+function rangeOfNumbers(startNum, endNum) {
+  if(startNum > endNum)
+  return [];
+  const arr = rangeOfNumbers(startNum + 1, endNum);
+  arr.unshift(startNum);
+  return arr;
+};
+
+// 113 tasks done
